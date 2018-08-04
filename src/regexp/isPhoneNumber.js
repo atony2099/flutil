b957e82acfaf88a79982b97d5abd7daa08857a8c
Fenglin Tang <atony2099@gmail.com>
2018-08-04 16:50:35 +0800

    fix

--- src/regexp/isPhoneNumber.js
+++ src/regexp/isPhoneNumber.js
@@ -2,11 +2,11 @@
  * @Author: atony2099 
  * @Date: 2018-08-02 11:34:01 
  * @Last Modified by: atony2099
- * @Last Modified time: 2018-08-02 11:40:43
+ * @Last Modified time: 2018-08-04 16:50:23
  */
 
 function isPhoneNumber(numStr){
-  return  /^1[3456789]\d{19}$/.test(numStr)
+  return  /^1[3456789]\d{9}$/.test(numStr)
 } 
 
 module.exports = isPhoneNumber
